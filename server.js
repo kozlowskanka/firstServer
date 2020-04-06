8a3b3342af8b8858f34b5cd973584514bc52608b
Anka Kozłowska <kozlowskanka@gmail.com>
2020-04-06 19:24:30 +0200

    Express.static added

--- server.js
+++ server.js
@@ -10,30 +10,32 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use(express.static(path.join(__dirname + '/public')));
+
 app.get('/', (req, res) => {
-  res.show('index.html');
+    res.show('index.html');
 });
 
 app.get('/about', (req, res) => {
-  res.show('about.html');
+    res.show('about.html');
 });
 
 app.get('/contact', (req, res) => {
-  res.show('contact.html');
+    res.show('contact.html');
 });
 
 app.get('/info', (req, res) => {
-  res.show('info.html');
+    res.show('info.html');
 });
 
 app.get('/history', (req, res) => {
-  res.show('history.html');
+    res.show('history.html');
 });
 
 app.use((req, res) => {
-  res.status(404).send('404 not found...');
+    res.status(404).send('404 not found...');
 })
 
 app.listen(8000, () => {
-  console.log('Server is running on port: 8000');
+    console.log('Server is running on port: 8000');
 });
