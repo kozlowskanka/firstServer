27c69e9444543ecfffe3b9727459d7bfa86993b2
Anka Kozłowska <kozlowskanka@gmail.com>
2020-04-12 12:50:36 +0200

    Add basic structure of form

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.engine('hbs', hbs({
 app.set('view engine', 'hbs');
 
 app.use(express.static(path.join(__dirname + '/public')));
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
     res.render('index');
@@ -37,6 +38,10 @@ app.get('/history', (req, res) => {
 app.get('/hello/:name', (req, res) => {
     res.render('hello', { name: req.params.name });
   })
+
+  app.post('/contact/send-message', (req, res) => {
+    res.json(req.body);
+  });
 
 app.use((req, res) => {
     res.status(404).send('404 not found...');
